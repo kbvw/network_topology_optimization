39c4ee3c374c0677cae9ace642fc25ee23bcf311
kbvw <kbvw@pm.me>
2022-05-15 17:22:28 +0200

    Update to re-exports

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from .core.core import adjacent, children, parents
 from .core.core import neighborhood, descendants, ancestors
-from .core.core import explore, reach, area, unique
+from .core.core import explore, reach, area
 
 from .core import abc
 from .core import collections
+from .core import itertools
