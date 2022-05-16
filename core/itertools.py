0542479761f0d73321b7a12ba4495beb8218804a
kbvw <kbvw@pm.me>
2022-05-16 14:27:53 +0200

    Distribute and supplement functions, plus change of splits function into a generator

--- core/itertools.py
+++ core/itertools.py
@@ -6,14 +6,18 @@ from typing import TypeVar
 A = TypeVar('A', bound=Hashable)
 B = TypeVar('B', bound=Hashable)
 
+C = TypeVar('C')
+
+Split = tuple[frozenset[A], ...]
+
 def chainmap(f: Callable[[A], Iterable[B]], xs: Iterable[A]) -> Iterator[B]:
-    """Chain of iterables from a map."""
+    """Chain of iterables from a map of f over xs."""
     
     return chain.from_iterable(map(f, xs))
 
 def unique(xs: Iterable[A],
            exclude: Set[A] = frozenset()) -> Generator[A, None, set[A]]:
-    """Unique elements from an iterable."""
+    """Unique elements from xs."""
     
     seen: set[A] = set()
     for x in xs:
@@ -24,8 +28,8 @@ def unique(xs: Iterable[A],
 
 def splits(xs: Set[A],
            min_size: int,
-           max_splits: int) -> Iterator[Iterable[frozenset[A]]]:
-    """All possible ordered splits of an iterable into disjoint subsets."""
+           max_splits: int) -> Iterator[Split[A]]:
+    """All possible ordered splits of xs into disjoint subsets."""
     
     if (len(xs) >= 2 * min_size) and (max_splits > 1):
         
@@ -35,7 +39,25 @@ def splits(xs: Set[A],
         prod = (product([ys], splits(xs - ys, min_size, max_splits - 1))
                 for ys in yss)
         
-        return ((split, *subsplits) for p in prod for split, subsplits in p)
+        yield from ((split, *subsplits) for p in prod 
+                    for split, subsplits in p)
     
     else:
-        return iter([tuple([frozenset(xs)])])
+        yield tuple([frozenset(xs)])
+
+def distribute(ys: Iterable[A], 
+               xsss: Iterable[Split[A]]) -> Iterator[Split[A]]:
+    """All possible distributions of ys over xss for all xss in xsss."""
+    
+    ys = iter(ys)
+    try:
+        y = next(ys)
+        zsss = chain.from_iterable(supplement(xss, y) for xss in xsss)
+        yield from distribute(ys, zsss)
+    except StopIteration:
+        yield from xsss
+
+def supplement(xss: Split[A], y: A) -> Iterator[Split[A]]:
+    """All possible ways to add y to one xs in xss."""
+    
+    return ((*xss[:n], xss[n] | {y}, *xss[n + 1:]) for n in range(len(xss)))
