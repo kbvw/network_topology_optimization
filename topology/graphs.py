660932b96e5a8632aa18c9f19a1a87c6a0aca295
kbvw <kbvw@pm.me>
2022-05-21 20:12:33 +0200

    Adjacency list of topology

--- topology/graphs.py
+++ topology/graphs.py
@@ -62,10 +62,11 @@ def degree_list(a_list: AList) -> DList:
     
     return {n: degree(a_list, n) for n in a_list}
 
-def topology(a_list: AList, coords: Topology) -> AList:
-    """Adjacency list of topology."""
+def en_from_ne(ne_list: NEList) -> ENList:
+    """Element-node list from a node-element list."""
     
-    pass
+    return {e: set(n for n in ne_list if e in ne_list[n]) 
+            for e in chain.from_iterable(ne_list.values())}
 
 def sub_nodes(n: N, coords: Topology) -> SubNs:
     """Tuple of sub-nodes resulting from a node split."""
@@ -73,7 +74,32 @@ def sub_nodes(n: N, coords: Topology) -> SubNs:
     return ((n, i) for i in range(len(coords.n_coord[n])))
 
 def sub_connections(n: N, en_list: ENList, coords: Topology) -> SubCs:
-    """Tuple of elements."""
+    """Tuple of sets of elements resulting from a node split."""
     
     guard = lambda e: (e in en_list) and (e not in coords.e_coord)
     return (frozenset(filter(guard, es)) for es in coords.n_coord[n][1])
+
+def topology_a_list(a_list: AList, coords: Topology) -> AList:
+    """Adjacency list of topology."""
+    
+    ne = ne_connection_list(a_list)
+    en = en_connection_list(a_list)
+    
+    splits = (zip(sub_nodes(n, coords), sub_connections(n, en, coords))
+              for n in ne if n in coords.n_coord)
+    
+    split_ne = {n: es for n, es in chain.from_iterable(splits)}
+    unsplit_ne = {n: es for n, es in ne.items() if n not in coords.n_coord}
+    
+    topo_ne = split_ne | unsplit_ne
+    topo_en = en_from_ne(topo_ne)
+    
+    topo_a: dict[N, dict[N, set[E]]] = {}
+    for n in topo_ne:
+        topo_a[n] = {}
+        for e in topo_ne[n]:
+            nbs = (nb for nb in topo_en[e] if not nb == n)
+            for nb in nbs:
+                topo_a[n].setdefault(nb, set()).add(e)
+    
+    return topo_a
