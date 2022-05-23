c076865e2ce6002635a8c3ebcba4591c5b4ae97b
kbvw <kbvw@pm.me>
2022-05-23 15:37:39 +0200

    Scaffolding

--- grid/powerflow.py
+++ grid/powerflow.py
@@ -1,36 +1,64 @@
 import numpy as np
-from scipy.sparse import csr_matrix, hstack, vstack # type: ignore
-from scipy.sparse.linalg import splu # type: ignore
 
-from collections.abc import Hashable, Iterable, Mapping, Sequence, Set
+from scipy.sparse import coo_array, csr_array, hstack # type: ignore
+from scipy.sparse.linalg import splu, SuperLU # type: ignore
+
+from collections.abc import Hashable, Mapping
 from typing import NamedTuple
 from numpy.typing import NDArray
 
 from itertools import product, chain
+from functools import reduce
+
+from ..core.itertools import unique
 
 from ..topology.coords import Topology
 
-C = Hashable
 N = Hashable
-
+C = Hashable
 L = Hashable
 G = Hashable
 
-AList = Mapping[N, Mapping[N, Iterable[C]]]
-NEList = Mapping[N, Iterable[L | G]]
-YList = Mapping[C, complex]
-SlackList = Mapping[G, float]
+CNList = dict[C, frozenset[N]]
+YList = dict[C, complex]
+LNList = dict[L, N]
+GNList = dict[G, N]
+SlackList = dict[G, float]
 
 class Grid(NamedTuple):
-    a_list: AList
-    ne_list: NEList
+    cn_list: CNList
     y_list: YList
+    ln_list: LNList
+    gn_list: GNList
     slack_list: SlackList
+
+B = Hashable
+
+CBList = dict[C, frozenset[B]]
+LBList = dict[L, B]
+GBList = dict[G, B]
+
+class BusSplit(NamedTuple):
+    cb_list: CBList
+    lb_list: LBList
+    gb_list: GBList
+
+BIndex = tuple[B, ...]
+SIndex = dict[B, float]
+YIndex = dict[frozenset[B], float]
+
+CIndex = dict[frozenset[B], frozenset[C]]
+
+class GridIndex(NamedTuple):
+    pv_idx: BIndex
+    pq_idx: BIndex
+    s_idx: SIndex
+    y_idx: YIndex
 
 PList = Mapping[L | G, float]
 QList = Mapping[L, float]
-AngList = Mapping[N, float]
-MagList = Mapping[N, float]
+AngList = Mapping[B, float]
+MagList = Mapping[B, float]
 FlowList = Mapping[C, complex]
 
 class GridData(NamedTuple):
@@ -43,19 +71,7 @@ class GridRes(NamedTuple):
     mag_list: MagList
     flow_list: FlowList
 
-B = Hashable
-
-BEList = Mapping[B, Iterable[L | G]]
-BIndex = Sequence[B]
-CIndex = Sequence[Set[C]]
-
-class GridIndex(NamedTuple):
-    be_list: BEList
-    pv_idx: BIndex
-    pq_idx: BIndex
-    c_idx: CIndex
-
-YMat = NDArray[np.float64]
+YMat = NDArray[np.complex64]
 
 PVec = NDArray[np.float64]
 QVec = NDArray[np.float64]
@@ -63,11 +79,12 @@ QVec = NDArray[np.float64]
 AngVec = NDArray[np.float64]
 MagVec = NDArray[np.float64]
 
+class PFInit(NamedTuple):
+    y_mat: YMat
+    bp_mat: SuperLU
+    bpp_mat: SuperLU
+
 class PFData(NamedTuple):
-    b_mat: YMat
-    g_mat: YMat
-    bp_mat: YMat
-    bpp_mat: YMat
     p_vec: PVec
     q_vec: QVec
     ang_vec: AngVec
@@ -78,91 +95,137 @@ def initialize(grid: Grid,
                grid_data: GridData) -> tuple[GridIndex, PFData]:
     pass
 
-def grid_index(grid: Grid,
-               topo: Topology,
-               grid_data: GridData) -> GridIndex:
+def slack_factors(grid: Grid, bus_split: BusSplit) -> SIndex:
     
-    be_list = bus_split(grid.ne_list, topo)
+    bs: dict[B, float]
+    bs = {b: 0. for b in unique(bus_split.gb_list.values())}
     
-    pv_idx, pq_idx = bus_types(be_list, grid_data)
-    
-    c_idx = connections(be_list, grid.y_list)
-    
-    return GridIndex(be_list, pv_idx, pq_idx, c_idx)
+    r = lambda bs, b: bs | {bs[b[0]] + grid.slack_list[b[1]]}
+    return reduce(r, bus_split.gb_list.items(), bs)
 
-def connections(be_list: BEList, y_list: YList) -> CIndex:
+def admittance(grid: Grid, bus_split: BusSplit) -> YIndex:
     
-    css = ({e for e in be_list[f] if e in be_list[t]}
-           for f, t in product(be_list, be_list))
+    bps: dict[frozenset[B], float]
+    bps = {bp: 0. for bp in unique(bus_split.cb_list.values())}
     
-    return tuple(filter(lambda cs: len(cs) > 0, css))
+    r = lambda bps, bp: bps | {bps[bp[0]] + grid.y_list[bp[1]]}
+    return reduce(r, bus_split.cb_list.items(), bps)
 
-def bus_types(be_list: BEList,
-              grid_data: GridData) -> tuple[BIndex, BIndex]:
+def bus_types(grid: Grid, bus_split: BusSplit) -> tuple[BIndex, BIndex]:
     
-    pv_idx = tuple(n for n in be_list 
-                     if any(e in grid_data.mag_list for e in be_list[n]))
-    pq_idx = tuple(n for n in be_list
-                     if all(e not in grid_data.mag_list for e in be_list[n]))
+    pv_idx = tuple(unique(n for n in bus_split.gb_list))
+    pq_idx = tuple(unique(n for n in bus_split.lb_list))
     
     return (pv_idx, pq_idx)
 
-def bus_split(ne_list: NEList,
-              topo: Topology) -> BEList:
+def bus_split(grid: Grid, topo: Topology) -> BusSplit:
     
-    unsplit = {n: {e for e in es if e not in topo.e_coord}
-               for n, es in ne_list.items() if n not in topo.n_coord}
-    split = {(n, i): es for n in topo.n_coord 
-             for i, es in enumerate(topo.n_coord[n][1])}
+    splits = {(n, i): [e for e in es if not e in topo.e_coord]
+              for n in topo.n_coord
+              for i, es in enumerate(topo.n_coord[n][1])}
     
-    return unsplit | split
+    l_splits = {e: b for b, es in splits.items()
+                for e in es if e in grid.ln_list}
+    g_splits = {e: b for b, es in splits.items()
+                for e in es if e in grid.gn_list}
+    
+    bs = lambda e: ({b for b in splits if e in splits[b]}
+                    | {b for b in grid.cn_list[e] if not b in topo.n_coord})
+    c_splits = {e: frozenset(bs(e)) 
+                for e in chain.from_iterable(splits.values())
+                if e in grid.cn_list}
+    
+    return BusSplit(cb_list = grid.cn_list | c_splits,
+                    lb_list = grid.ln_list | l_splits,
+                    gb_list = grid.gn_list | g_splits)
 
 def pf_data(grid_data: GridData,
             grid_idx: GridIndex) -> PFData:
     pass
 
-def a_mat(b_idx: BIndex, be_list: BEList, y_list: YList) -> YMat:
+def pf_init(grid: Grid, grid_idx: GridIndex) -> PFInit:
     
-    cs = ((f, t, sum(y_list[e] for e in be_list[t] if e in be_list[t]))
-          for f, t in product(b_idx, b_idx))
+    y = laplacian(grid_idx.pv_idx + grid_idx.pq_idx, grid_idx.y_idx)
     
-    rows, cols, data = zip(*filter(lambda c: c[2] != 0, cs))
+    bp = bp_mat(grid_idx)
     
-    return csr_matrix((data, (rows, cols)), shape=2*[len(be_list)])
+    bpp = bpp_mat(grid_idx)
+    
+    return PFInit(y_mat=y, bp_mat=splu(bp), bpp_mat=splu(bpp))
 
-def laplacian(b_idx: BIndex, be_list: BEList, y_list: YList) -> YMat:
+def a_mat(b_idx: BIndex, y_idx: YIndex) -> YMat:
     
-    a = a_mat(b_idx, be_list, y_list)
+    cs = ((f[0], t[0], y_idx[frozenset([f[1], t[1]])]) 
+          for f, t in product(enumerate(b_idx), enumerate(b_idx)) 
+          if frozenset([f[1], t[1]]) in y_idx)
     
-    return csr_matrix(a.sum(axis=0) - a)
+    rows, cols, data = zip(*cs)
+    
+    return csr_array((data, (rows, cols)), shape=2*[len(b_idx)])
 
-def bp_mat(pv_idx: BIndex, 
-           pq_idx: BIndex,
-           be_list: BEList, 
-           y_list: YList, 
-           slack_list: SlackList) -> YMat:
+def laplacian(b_idx: BIndex, y_idx: YIndex) -> YMat:
     
-    grounded_lap = laplacian([pv_idx] + [pq_idx], be_list, y_list)[1:, 1:]
-    padded_lap = csr_matrix([np.zeros(len(pv_idx)-1), *grounded_lap])
-    slack = np.array(sum(slack_list[e] for e in be_list[b]) 
-                     for b in pv_idx)
+    a = a_mat(b_idx, y_idx)
+    idx = np.arange(a.shape[0])
+    diag = csr_array((a.sum(axis=1), (idx, idx)), shape=a.shape)
     
-    return np.imag(csr_matrix([slack, *padded_lap.T]).T)
+    return diag - a
 
-def bpp_mat(pv_idx: BIndex,
-            pq_idx: BIndex, 
-            be_list: BEList, 
-            y_list: YList) -> YMat:
+def slack_array(pv_idx: BIndex, s_idx: SIndex) -> NDArray[np.float64]:
     
-    lap = laplacian([pv_idx] + [pq_idx], be_list, y_list)
+    slack = [(b[0], s_idx[b[1]]) for b in enumerate(pv_idx)
+             if b[1] in s_idx]
     
-    return np.imag(lap[len(pv_idx):, len(pv_idx):])
+    rows, data = zip(*slack)
+    cols = np.zeros(len(slack))
+    
+    return csr_array((data, (rows, cols)), shape=[len(slack), 1])
 
-def p(ang_vec: AngVec, mag_vec: MagVec, b_mat: YMat, g_mat: YMat) -> PVec:
+def bp_mat(grid_idx: GridIndex) -> YMat:
+    
+    lap = laplacian(grid_idx.pv_idx + grid_idx.pq_idx, grid_idx.y_idx)
+    
+    slack = slack_array(grid_idx.pv_idx, grid_idx.s_idx)
+    
+    return csr_array(hstack([slack, np.imag(lap[:, 1:])]))
+
+def bpp_mat(grid_idx: GridIndex) -> YMat:
+    
+    lap = laplacian(grid_idx.pv_idx + grid_idx.pq_idx, grid_idx.y_idx)
+    
+    pq_start = len(grid_idx.pv_idx)
+    
+    return np.imag(lap[pq_start:, pq_start:])
+
+def p(ang_vec: AngVec, mag_vec: MagVec, y_mat: YMat) -> PVec:
+    
+    cs = coo_array(y_mat)
+    
+    ang_diffs = ang_vec[cs.row] - ang_vec[cs.col]
+    mags = mag_vec[cs.col]
+    
+    bs = np.imag(cs.data)*np.sin(ang_diffs)
+    gs = np.real(cs.data)*np.cos(ang_diffs)
+    
+    amps = csr_array((mags*(bs + gs), (cs.row, cs.col))).sum(axis=1)
+    
+    return mag_vec*amps
+
+def q(ang_vec: AngVec, mag_vec: MagVec, y_mat: YMat) -> QVec:
+    
+    cs = coo_array(y_mat)
+    
+    ang_diffs = ang_vec[cs.row] - ang_vec[cs.col]
+    mags = mag_vec[cs.col]
+    
+    bs = -np.imag(cs.data)*np.cos(ang_diffs)
+    gs = np.real(cs.data)*np.sin(ang_diffs)
+    
+    amps = csr_array((mags*(bs + gs), (cs.row, cs.col))).sum(axis=1)
+    
+    return mag_vec*amps
+
+def fdpf(pf_data: PFData, pf_init: PFInit):
     pass
-
-def q(ang_vec: AngVec, mag_vec: MagVec, b_mat: YMat, g_mat: YMat) -> QVec:
-    pass
-
 
 
