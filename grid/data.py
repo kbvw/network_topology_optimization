92cfb160e657869a21a893bb4df4e9e980f153a8
kbvw <kbvw@pm.me>
2022-05-24 17:03:48 +0200

    Bus-split as a grid object and admittances/slack separately in gridparams

--- grid/data.py
+++ grid/data.py
@@ -14,17 +14,26 @@ L = Hashable
 G = Hashable
 B = Hashable
 
-class Grid(NamedTuple):
-    cn_list: dict[C, frozenset[N]]
-    y_list: dict[C, complex]
-    ln_list: dict[L, N]
-    gn_list: dict[G, N]
-    slack_list: dict[G, float]
+CNList = dict[C, frozenset[N]]
+LNList = dict[L, N]
+GNList = dict[G, N]
 
-class BusSplit(NamedTuple):
-    cb_list: dict[C, frozenset[B]]
-    lb_list: dict[L, B]
-    gb_list: dict[G, B]
+Admittances = dict[C, complex]
+SlackFactors = dict[G, float]
+
+class Grid(NamedTuple):
+    cn_list: CNList
+    ln_list: LNList
+    gn_list: GNList
+
+class GridParams(NamedTuple):
+    y_list: Admittances
+    s_list: SlackFactors
+
+# class BusSplit(NamedTuple):
+#     cb_list: dict[C, frozenset[B]]
+#     lb_list: dict[L, B]
+#     gb_list: dict[G, B]
 
 BIndex = tuple[B, ...]
 SIndex = dict[B, float]
@@ -36,30 +45,42 @@ class GridIndex(NamedTuple):
     s_idx: dict[B, float]
     y_idx: dict[frozenset[B], float]
 
-def slack_factors(grid: Grid, bus_split: BusSplit) -> SIndex:
+class PFIndex(NamedTuple):
+    pv_idx: tuple[B, ...]
+    pq_idx: tuple[B, ...]
+    s_idx: dict[B, float]
+    y_idx: dict[frozenset[B], float]
+
+def apply_topology(grid: Grid, topo: Topology) -> Grid:
+    pass
+
+def pf_index(grid: Grid, grid_params: GridParams) -> PFIndex:
+    pass
+
+def slack_factors(grid: Grid, grid_params: GridParams) -> SIndex:
     
     bs: dict[B, float]
-    bs = {b: 0. for b in unique(bus_split.gb_list.values())}
+    bs = {b: 0. for b in unique(grid.gn_list.values())}
     
-    r = lambda bs, b: bs | {bs[b[0]] + grid.slack_list[b[1]]}
-    return reduce(r, bus_split.gb_list.items(), bs)
+    r = lambda bs, b: bs | {bs[b[0]] + grid_params.s_list[b[1]]}
+    return reduce(r, grid.gn_list.items(), bs)
 
-def admittance(grid: Grid, bus_split: BusSplit) -> YIndex:
+def admittances(grid: Grid, grid_params: GridParams) -> YIndex:
     
     bps: dict[frozenset[B], float]
-    bps = {bp: 0. for bp in unique(bus_split.cb_list.values())}
+    bps = {bp: 0. for bp in unique(grid.cn_list.values())}
     
-    r = lambda bps, bp: bps | {bps[bp[0]] + grid.y_list[bp[1]]}
-    return reduce(r, bus_split.cb_list.items(), bps)
+    r = lambda bps, bp: bps | {bps[bp[0]] + grid_params.y_list[bp[1]]}
+    return reduce(r, grid.cn_list.items(), bps)
 
-def bus_types(grid: Grid, bus_split: BusSplit) -> tuple[BIndex, BIndex]:
+def bus_types(grid: Grid, grid_params: GridParams) -> tuple[BIndex, BIndex]:
     
-    pv_idx = tuple(unique(n for n in bus_split.gb_list))
-    pq_idx = tuple(unique(n for n in bus_split.lb_list))
+    pv_idx = tuple(unique(n for n in grid.gn_list))
+    pq_idx = tuple(unique(n for n in grid.ln_list))
     
     return (pv_idx, pq_idx)
 
-def bus_split(grid: Grid, topo: Topology) -> BusSplit:
+def bus_split(grid: Grid, topo: Topology) -> Grid:
     
     splits = {(n, i): [e for e in es if not e in topo.e_coord]
               for n in topo.n_coord
@@ -76,6 +97,6 @@ def bus_split(grid: Grid, topo: Topology) -> BusSplit:
                 for e in chain.from_iterable(splits.values())
                 if e in grid.cn_list}
     
-    return BusSplit(cb_list = grid.cn_list | c_splits,
-                    lb_list = grid.ln_list | l_splits,
-                    gb_list = grid.gn_list | g_splits)
+    return Grid(cn_list = grid.cn_list | c_splits,
+                ln_list = grid.ln_list | l_splits,
+                gn_list = grid.gn_list | g_splits)
